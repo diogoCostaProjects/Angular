82be768ce26224e45e80363510fb7bc05741cce0
diogoCostaProjects <diogocosta.jar@gmail.com>
2018-10-18 07:17:37 -0300

    Revisao TypeScript

---
+++ my-app/hello-typescript/main.ts
@@ -0,0 +1,19 @@
+var minhaVar  = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x+y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(valor => valor * 2); // ES 2015
+
+class Matematica{
+    soma (x,y){
+        return x+y;
+    }
+}
+var n1: String = 'asaoapo'; // tipágem de variáveis no TypeScript
+var n2: number = 1;
